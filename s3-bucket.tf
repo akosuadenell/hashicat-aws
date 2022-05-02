f30482af7a007af827e587971e84e2bb62ed70af
Denell Cr <denell@hashicorp.com>
2022-05-02 23:26:48 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/DenellC/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix = "Denell-Cr"
+  bucket_prefix = "Denell_Cr"
 }
